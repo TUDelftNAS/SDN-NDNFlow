508388ffe472e59d6c5a3758aa407a4d7df698e4
Niels van Adrichem <nvanadrichem@outlook.com>
2015-03-24 14:25:20 +0100

    Changed software name in licensing notes.

--- CCNx/src/ClientManager.java
+++ CCNx/src/ClientManager.java
@@ -16,7 +16,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with OpenNetMon. If not, see <http://www.gnu.org/licenses/>.
+# along with NDNFlow. If not, see <http://www.gnu.org/licenses/>.
 **/
 
 import java.io.BufferedReader;
--- CCNx/tools/compile.sh
+++ CCNx/tools/compile.sh
@@ -15,7 +15,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with OpenNetMon. If not, see <http://www.gnu.org/licenses/>.
+# along with NDNFlow. If not, see <http://www.gnu.org/licenses/>.
 
 JAR_FILE=ccn.jar
 JAVA_HOME=${JAVA_HOME:=/usr}
